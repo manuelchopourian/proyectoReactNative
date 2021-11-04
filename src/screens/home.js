5cd044aa287a6923d8cf278f3efabf155bef573b
manuelchopourian <manuelchopourian@Manuels-MacBook-Air.local>
2021-11-04 10:38:11 -0300

    arreglo carpetas

---
+++ src/screens/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {View, StyleSheet} from 'react-native';
+
+class Home extends Component {
+    render() {
+        return (
+            <View>
+                
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({})
+
+export default Home;
